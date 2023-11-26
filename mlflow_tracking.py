7904cbfb74e230c440d436a206502400baf01e6a
Niklas Weiand <niklas.weiand@stud.h-da.de>
2023-11-26 20:57:43 +0100

    added regression

---
+++ mlflow_tracking.py
@@ -0,0 +1,35 @@
+import mlflow
+from mlflow.models import infer_signature
+from regression import IrisRegression
+
+
+irreg = IrisRegression()
+params, accuracy, lr = irreg.train_model()
+
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+
+# Create a new MLflow Experiment
+mlflow.set_experiment("MLflow IrisRegression")
+
+# Start an MLflow run
+with mlflow.start_run():
+    # Log the hyperparameters
+    mlflow.log_params(params)
+
+    # Log the loss metric
+    mlflow.log_metric("accuracy", accuracy)
+
+    # Set a tag that we can use to remind ourselves what this run was for
+    mlflow.set_tag("Training Info", "Basic LR model for iris data")
+
+    # Infer the model signature
+    signature = infer_signature(irreg.X_train, lr.predict(irreg.X_train))
+
+    # Log the model
+    model_info = mlflow.sklearn.log_model(
+        sk_model=lr,
+        artifact_path="iris_model",
+        signature=signature,
+        input_example=irreg.X_train,
+        registered_model_name="tracking-quickstart",
+    )
---
+++ regression.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+
+
+class IrisRegression:
+
+    def __init__(self):
+        self.X, self.y = datasets.load_iris(return_X_y=True)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+        self.X, self.y, test_size=0.2, random_state=42
+    )
+        self.params = {
+        "solver": "lbfgs",
+        "max_iter": 1000,
+        "multi_class": "auto",
+        "random_state": 8888,
+    }
+
+
+    def train_model(self):
+        lr = LogisticRegression(**self.params)
+        lr.fit(self.X_train, self.y_train)
+        y_pred = lr.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        return(self.params, accuracy,lr)
